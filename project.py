84a8e5ac970c2f33358264c0f15c8d525ecd536e
princeemtiaz <111088803+princeemtiaz@users.noreply.github.com>
2023-09-16 13:46:18 +0600

    user can revered seat

--- project.py
+++ project.py
@@ -54,9 +54,50 @@ class Phitron:
             self.total_bus_list.append(new_bus)
             print("\nBus Added Successfully")
 
-company=Phitron()
-company.add_bus()
+# company=Phitron()
+# company.add_bus()
             
+class Counter(Phitron):
+    def reservation(self):
+        try:
+            bus_no = int(input("Enter Bus No: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid Bus No (an integer).")
+            return  # Exit the method if the input is not a valid integer
+
+        for bus in self.total_bus_list:
+            if bus_no == bus.coach:
+                passenger = input("Enter Your Name: ")
+                seat_no = int(input("Enter Seat No: "))
+
+                if seat_no > 20:
+                    print("No Seat Available....!!! ")
+
+                elif bus.seat[seat_no - 1] != "Empty":
+                    print("Seat Already Booked....!!! ")
+
+                else:
+                    bus.seat[seat_no - 1] = passenger
+        for bus in self.total_bus_list:
+            print(bus.seat)
+
+# Create an instance of the Bus class to use it as a base class for Counter
+company = Phitron()
+company.add_bus()
+
+doremmon = Counter()
+doremmon.reservation()
+
+
+
+
+                
+                
+                
+            
+        
+        
+    
         
         
         
